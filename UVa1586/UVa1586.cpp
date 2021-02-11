00ab684bbfe04a904a1c7c8c761cce819e767a20
YunXiuRZ <70698588+YunXiuRZ@users.noreply.github.com>
2021-02-11 21:35:37 +0800

    2/11

--- UVa1586/UVa1586.cpp
+++ UVa1586/UVa1586.cpp
@@ -1,15 +1,37 @@
 #include<iostream>
 #include<string>
-#include<map>
+#include<unordered_map>
+#include<iomanip>
 using namespace std;
 
 int main(){
+    int n;
+    cin>>n;
     string s;
-    while(cin>>s){
-        map<char, int> result = {{"C", 0},
-                                 {"H", 0},
-                                 {"N", 0},
-                                 {"O", 0}};
-        
+    getline(cin, s);
+    while (n--) {
+        unordered_map<char, int> count;
+        char element  = ' ';
+        int amount = 0;
+        getline(cin, s);
+        for (char c : s) {
+            if(isdigit(c)){
+                amount = amount*10 + c-48;
+            }else{
+                if(amount == 0)
+                    amount++;
+                count[element]+=amount;
+                element = c;
+                amount = 0;
+            }
+        }
+        if(amount == 0)
+            amount++;
+        count[element]+=amount;
+        double result = count['C'] * 12.01 +
+                        count['H'] * 1.008 +
+                        count['N'] * 14.01 +
+                        count['O'] * 16;
+        cout<<fixed<<setprecision(3)<<result<<endl;
     }
 }
